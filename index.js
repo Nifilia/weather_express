526d1e2a95c24e91186990eebd4fb06f266e5e27
Nifilia <anderokore@gmail.com>
2022-02-02 15:23:46 +0200

    part 3

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = "ae283fd21026f903724fe0bd7811dee2";
+let city = 'Tartu'
+
 app.get('/', function(req, res){
-	res.render('index')
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	then((response) =>{
+		return response.json()
+	})
+	then((data) =>{
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273.15)
+		console.log(description)
+		console.log(city)
+		console.log(temp)
+	})
 })
 
 app.listen(3000)
